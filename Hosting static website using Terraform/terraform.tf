3943e841b88e56799c17d0a5d698a5d8ab37a560
Arbaz Naeem <arbaznaeem555@gmail.com>
2025-12-18 23:36:08 +0500

    added terraform.tf for var

---
+++ Hosting static website using Terraform/terraform.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "The AWS region."
+  type        = string
+}
+variable "length_for_random_id" {
+  description = "The length of the random id"
+  type        = number
+}
+
+
+
